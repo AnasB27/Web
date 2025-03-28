0d3ad86da8436297beb8d8dff8cf07d59d3d5c23
G2ll0u <virgil.gregoire@viacesi.fr>
2025-03-28 10:15:02 +0100

    company functions (empty)

---
+++ src/Models/company.php
@@ -0,0 +1,19 @@
+<?php
+
+class CompanyModel {
+
+    public function create_company() {
+
+}
+    public function read_company() {
+
+    }
+
+    public function update_company() {
+
+    }
+    public function delete_company() {
+        
+    }
+}
+?>
